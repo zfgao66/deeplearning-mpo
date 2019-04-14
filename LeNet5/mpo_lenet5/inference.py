6ee750f8d39b8b9518809b209b130452ad4b2f05
zfgao <46995180+zfgao66@users.noreply.github.com>
2019-04-14 09:35:13 +0800

    Add files via upload

--- LeNet5/mpo_lenet5/inference.py
+++ LeNet5/mpo_lenet5/inference.py
@@ -1,85 +1,84 @@
-"""
-This network we change the lenet5's full-connected layer with mpo structure and 
-set the mpo structure as 2-5-6-2, 2-3-7-2,1-5-2-1 and rank=4.
-compress ratio is 10.9
-"""
+
 import numpy as np
 import tensorflow as tf
 import tt
-from hyper_parameters import *
-
-input_node  = FLAGS.input_node
-output_node = FLAGS.output_node
-
+from hyperprameter import *
 
 r_1 = FLAGS.tt_ranks_1
 r_2 = FLAGS.tt_ranks_2
+r_3 = FLAGS.tt_ranks_3
 
-#TTO_layer1
-inp_modes1 =  [2,5,6,2]          
-out_modes1 =  [2,3,7,2]        
-mat_rank1  =  [1,r_1,r_1,r_1,1]
+input_node=FLAGS.input_node
+output_node=FLAGS.output_node
 
-#TTO_layer2
-inp_modes2 = [2,3,7,2]    
-out_modes2 = [1,5,2,1]        
-mat_rank2 =  [1,r_2,r_2,r_2,1]
+# TTO_layer2
+inp_modes2 = [2, 10, 10, 2]
+out_modes2 = [2, 5, 6, 2]  
+mat_rank2 = [1, r_1, r_1, r_1, 1]
 
+# TTO_layer3
+inp_modes3 = [2, 5, 6, 2]
+out_modes3 = [2, 3, 7, 2]  
+mat_rank3 = [1, r_2, r_2, r_2, 1]
+
+# TTO_layer4
+inp_modes4 = [2, 3, 7, 2]  
+out_modes4 = [1, 5, 2, 1]
+mat_rank4 = [1, r_3, r_3, r_3, 1]
 
 
 def conv2d(x, W):
-  return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='VALID')
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='VALID')
+
+
 def max_pool_2x2(x):
-  return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
-                        strides=[1, 2, 2, 1], padding='SAME')
+    return tf.nn.max_pool(x, ksize=[1, 2, 2, 1],
+                          strides=[1, 2, 2, 1], padding='SAME')
+
+
 def weight_variable(shape):
-  initial = tf.truncated_normal(shape, stddev=0.1)
-  return tf.Variable(initial)
+    weights = tf.Variable(tf.truncated_normal(shape, stddev=0.1))
+    return weights
+
 
 def bias_variable(shape):
-  initial = tf.constant(0.0, shape=shape)
-  return tf.Variable(initial)
+    initial = tf.constant(0.0, shape=shape)
+    return tf.Variable(initial)
+
 
 def inference(inputs):
-	W_conv1 = weight_variable([5, 5, 1, 6])
-	b_conv1 = bias_variable([6])
-	
-	x_image = tf.reshape(inputs, [-1,28,28,1])
-	#28x28x1
-	h_conv1 = tf.nn.relu(tf.nn.conv2d(x_image, W_conv1, strides=[1,1,1,1], padding='SAME') + b_conv1)
-	# 28x28x6
-	h_pool1 = max_pool_2x2(h_conv1)
-	#14x14x6
-	W_conv2 = weight_variable([5, 5, 6, 16])
-	b_conv2 = bias_variable([16])
-	
-	h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
-	#10x10x16
-	h_pool2 = max_pool_2x2(h_conv2)
-	#5x5x16
-	W_conv3 = weight_variable([5, 5, 16, 120])
-	b_conv3 = bias_variable([120])
-	h_conv3 = tf.nn.relu(conv2d(h_pool2,W_conv3) + b_conv3)
-	#1x1x120
-	inputs = tf.reshape(h_conv3,[-1, 120])
-	inputs = tf.nn.relu(tt.tto(inputs,
-							   np.array(inp_modes1,dtype=np.int32),
-							   np.array(out_modes1,dtype=np.int32),
-							   np.array(mat_rank1,dtype=np.int32),
-							   biases_initializer=None,
-							   scope='tt_scope_1'))
+    W_conv1 = weight_variable([5, 5, 1, 6])
+    b_conv1 = bias_variable([6])
 
-	output = tt.tto(inputs,
-							   np.array(inp_modes2,dtype=np.int32),
-							   np.array(out_modes2,dtype=np.int32),
-							   np.array(mat_rank2,dtype=np.int32),
-							   biases_initializer=None,
-							   scope='tt_scope_2')
-	return output
-
-	
-
-        
-        
-                        
+    x_image = tf.reshape(inputs, [-1, 28, 28, 1])
+    # 28x28x1
+    h_conv1 = tf.nn.relu(tf.nn.conv2d(x_image, W_conv1, strides=[1, 1, 1, 1], padding='SAME') + b_conv1)
+    # 28x28x6
+    h_pool1 = max_pool_2x2(h_conv1)
+    # 14x14x6
+    W_conv2 = weight_variable([5, 5, 6, 16])
+    b_conv2 = bias_variable([16])
+    h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
+    # 10x10x16
+    h_pool2 = max_pool_2x2(h_conv2)
+    # 5x5x16
+    inputs = tf.reshape(h_pool2,[-1,400])
+    inputs = tf.nn.relu(tt.tto(inputs,
+                               np.array(inp_modes1, dtype=np.int32),
+                               np.array(out_modes1, dtype=np.int32),
+                               np.array(mat_rank1, dtype=np.int32),
+                               scope='tt_scope_1'))
+    inputs = tf.nn.relu(tt.tto(inputs,
+                               np.array(inp_modes2, dtype=np.int32),
+                               np.array(out_modes2, dtype=np.int32),
+                               np.array(mat_rank2, dtype=np.int32),
+                               scope='tt_scope_2'))
     
+    output = tt.tto(inputs,
+                    np.array(inp_modes3, dtype=np.int32),
+                    np.array(out_modes3, dtype=np.int32),
+                    np.array(mat_rank3, dtype=np.int32),
+                    scope='tt_scope_3') 
+
+
+    return output
--- LeNet5/mpo_lenet5/train.py
+++ LeNet5/mpo_lenet5/train.py
@@ -1,67 +1,62 @@
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 """
 @author: zfgao
-
-This network is MPO-lenet5 in MNIST dataset 
-We set the lr=0.01 and use SGD optimizer
-To run this program you can just input 
-$ python train.py 
 """
 
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 import inference
-from hyper_parameters import *
+from hyperprameter import *
 
 BATCH_SIZE=FLAGS.batch_size
-TRAINING_STEPS=FLAGS.train_steps
-lr = FLAGS.learning_rate
+TRAINING_STEPS=FLAGS.global_step
+LEARNING_RATE_BASE=FLAGS.LEARNING_RATE_BASE
+LEARNING_RATE_DECAY=FLAGS.LEARNING_RATE_DECAY
+REGULARIZER_RATE=FLAGS.REGULARIZER_RATE
+MOVING_DECAY=0.99
+#seed =12345
+#tf.set_random_seed(seed)
 
 def mnist(inp):
-
     x=tf.placeholder(tf.float32,[None,inference.input_node],name='x-input')
     y_=tf.placeholder(tf.float32,[None,inference.output_node],name='y-input')
-
     y=inference.inference(x)
     global_step=tf.Variable(0,trainable=False)
-
-    ce=tf.nn.softmax_cross_entropy_with_logits(logits=y,labels=y_)
+    ce=tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y,labels=tf.argmax(y_,1))
     loss=tf.reduce_mean(ce)
-    tf.summary.scalar('loss',loss)
+    loss += tf.add_n([tf.nn.l2_loss(var) for var in tf.trainable_variables()]) * REGULARIZER_RATE
 
-    train_steps=tf.train.GradientDescentOptimizer(lr).minimize(loss,global_step=global_step)
+    learning_rate=tf.train.exponential_decay(LEARNING_RATE_BASE,
+                                             global_step,
+                                             inp.train.num_examples/BATCH_SIZE,
+                                             LEARNING_RATE_DECAY)
+    train_steps=tf.train.GradientDescentOptimizer(learning_rate).minimize(loss,global_step=global_step)
 
     correct_prediction=tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
     accuracy=tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
-    tf.summary.scalar('accuracy',accuracy)
 
-    merged = tf.summary.merge_all()
 
     with tf.Session() as sess:
         init_op = tf.global_variables_initializer()
         sess.run(init_op)
-        train_writer = tf.summary.FileWriter('./logs/train', sess.graph)
-        test_writer = tf.summary.FileWriter('./logs/test')                      
+        best_acc = 0
         for i in range(TRAINING_STEPS):
             xs,ys = inp.train.next_batch(BATCH_SIZE)
-            _,loss_value,step,summary = sess.run([train_steps,loss,global_step,merged],feed_dict={x:xs,y_:ys})
-            train_writer.add_summary(summary, i)
-            best_pre = 0.0
+            _,step,lr = sess.run([train_steps,global_step,learning_rate],feed_dict={x:xs,y_:ys})
             if i%1000 == 0:
-                summary, accuracy_score = sess.run([merged, accuracy], feed_dict={x:inp.test.images,y_:inp.test.labels})
-                test_writer.add_summary(summary, i)
+                accuracy_score = sess.run(accuracy, feed_dict={x:inp.test.images,y_:inp.test.labels})
+                print('step={},lr={}'.format(step,lr))
+                if best_acc< accuracy_score:
+                    best_acc = accuracy_score
                 print('Accuracy at step %s: %s' % (i, accuracy_score))
-                if (best_pre <= accuracy_score):
-                    best_pre = accuracy_score
-        print("After %s trainning step(s),best accuracy=%g" %(step,best_pre))
+        accuracy_score=sess.run(accuracy,feed_dict={x:inp.test.images,y_:inp.test.labels})
+        print("After %s trainning step(s),best accuracy=%g" %(step,best_acc))
+
+
 
 def main(argv=None):
-    inp=input_data.read_data_sets("./data/",one_hot=True)
+    inp=input_data.read_data_sets("./data/",validation_size=0,one_hot=True)
     mnist(inp)
-    
+
 if __name__=='__main__':
     tf.app.run()
-    
-    
-
-
